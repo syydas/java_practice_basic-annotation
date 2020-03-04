6850080c0c7a59099f271c227eaa47dbb471b209
Asuu <suqinzheng_0624@163.com>
2020-03-04 11:25:37 +0800

    [Asu] feat: add Limit annotation

--- annotation/src/main/java/com/thoughtworks/model/Student.java
+++ annotation/src/main/java/com/thoughtworks/model/Student.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.model;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -13,9 +12,17 @@ import java.lang.annotation.Target;
     String name() default "";
 }
 
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Limit {
+    int min() default 0;
+
+    int max() default 0;
+}
+
 @Alias(name = "自定义")
 public class Student {
     // 为age字段添加一个Limit注解, 表明age字段的最小值为1 最大值为140
+    @Limit(min = 1, max = 140)
     private int age;
-
 }
